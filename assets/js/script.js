059b3eb4befa531264f06aa5421fe1fba2226c2e
thoak90codes <adorahtho@gmail.com>
2023-03-30 01:11:57 -0500

    put questions into array. created for each loop for questions. appended elements to create player name input element. store score and player name into local storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,69 +1,139 @@
+var questionBox = document.querySelector("#question-box")
+
+var score = 0
+var questionCounter = 0
+
 var quizTimer = document.querySelector("#timer")
 var quizBox = document.querySelector(".main-quiz-box")
-var questionBox = document.querySelector("#question-box")
-var questionChoices = document.querySelector(".instructions")
-var startButton = document.querySelector(".start-button")
-var instructionsLi = document.querySelector(".instructions-li")
+var startButton = document.querySelector("#start-button")
+var instructionsP = document.querySelector(".instruction-p")
 
-var timeLeft = 10
+var timeLeft = 30
 
-var optionALi = document.querySelector("#optionA")
-var optionBLi = document.querySelector("#optionB")
-var optionCLi = document.querySelector("#optionC")
-var optionDLi = document.querySelector("#optionD")
+var questions = [
+  {
+    question: 'What is the primitive type for the following value, "Hello"?',
+    options: ["String", "Number", "Boolean", "Null",],
+    answer: "String",
+  },
+  {
+    question: "Where do we put the <script> element for JavaScript in the HTML?",
+    options: ["<head>", "<body>", "<footer>", "<div>"],
+    answer: "<body>",
+  },
+  {
+    question: "Which window popup allows you to type in an answer?",
+    options: ["alert","confirm", "prompt", "popup",],
+    answer: "prompt",
+  },
+  {
+    question: "Local Scope variables allows access to everything outside the block code.",
+    options: ["True", "False",],
+    answer: "False",
+  },
+  {
+    question: "What is JavaScript?",
+    options: ["A programming language that's a markup language.", "A programming language used to style the website.", "A programming language used to make websites more interactive.", "A programming language used to replace HTML and CSS.",],
+    answer: "A programming language used to make websites more interactive.",
+  }
+]
 
-var optionABtn = document.createElement("button")
-var optionBBtn = document.createElement("button")
-var optionCBtn = document.createElement("button")
-var optionDBtn = document.createElement("button")
-
-
-function Question1 (event) {
-  event.preventDefault(); 
+function quizQuestions() {
 
   startButton.style.display = "none"
+  instructionsP.textContent = ""
 
-  questionBox.textContent = "1. What primitive type is for the following value, 'Hello'."
-  instructionsLi.textContent = ""
-  optionABtn.textContent = "a. String"
-  optionBBtn.textContent = "b. Number"
-  optionCBtn.textContent = "c. Boolean"
-  optionDBtn.textContent = "d. Null"
+  var currentQuestion = questions[questionCounter]
+  questionBox.textContent = currentQuestion.question
+  
+  document.querySelector(".all-options").innerHTML = ""
+  
+  currentQuestion.options.forEach(function(option){
+    var button = document.createElement("button")
+    button.textContent = option
+    button.setAttribute("value", option)
+    button.addEventListener("click", function(event){
+      console.log(event.target.value)
+      if(event.target.value === currentQuestion.answer){
+        console.log('correct answer')
+        score++
+        console.log('score', score)
+      }else {
+        console.log('incorrect answer')
+        timeLeft-= 10
+      }
+      
+      questionCounter++
 
-  optionALi.appendChild(optionABtn)
-  optionBLi.appendChild(optionBBtn)
-  optionCLi.appendChild(optionCBtn)
-  optionDLi.appendChild(optionDBtn)
+      if(questionCounter < questions.length){
+        quizQuestions()
+      }else {
+        displayScore()
+      }
+    })
+  document.querySelector(".all-options").appendChild(button)
+  })
+}
 
-  var allOptionsBtn = [optionABtn, optionBBtn, optionCBtn, optionDBtn]
-  for (var i = 0; i < allOptionsBtn.length; i++) {
-    allOptionsBtn[i].setAttribute("style", "font-size: 20px; margin: 10px; width: 400px")
+var playerNameForm = document.createElement("form")
+var playerNameLabel = document.createElement("label")
+var playerNameInput = document.createElement("Input")
+var submitNameBtn = document.createElement("button")
+
+function displayScore() {
+  questionBox.textContent = "All done!"
+  document.querySelector(".all-options").style.display = ""
+
+  instructionsP.textContent = "Final Score: " + score +"/5"
+  localStorage.setItem("Score", score)
+
+  document.querySelector("#player-name").innerHTML = ""
+  document.querySelector("#player-name").appendChild(playerNameForm)
+
+  document.querySelector("form").appendChild(playerNameLabel)
+  playerNameLabel.textContent = "Enter Player Name: "
+
+  document.querySelector("form").appendChild(playerNameInput)
+
+  document.querySelector("form").appendChild(submitNameBtn)
+
+  submitNameBtn.textContent = "Submit"
+}
+
+submitNameBtn.addEventListener("click", function(event){
+  event.preventDefault();
+
+  var playerNameInputLS = playerNameInput.value
+  
+  if (playerNameInputLS === playerNameInput.value){
+    localStorage.setItem("playerName", playerNameInputLS)
   }
 
-  questionBox.setAttribute("style", "padding-left: 20px; padding-right: 20px")
-
-
-  startTimer()
-}
-
-
-function showCorrectResult (event) {
-  event.preventDefault()
-
-}
+  
+})
 
 function startTimer() {
   var timerInterval = setInterval(function() {
     timeLeft--;
+    if(timeLeft < 0) {
+      timeLeft = 0
+    }
     quizTimer.textContent = "Timer: " + timeLeft;
     if(timeLeft <= 5) {
       quizTimer.setAttribute("style", "color: red; font-size: 18px")
     }
     if (timeLeft === 0) {
       clearInterval (timerInterval);
+      displayScore()
     }
   }, 1000)
 }
 
+startButton.addEventListener("click", function(){
+  startTimer()
+  quizQuestions()
+})
 
-startButton.addEventListener("click", Question1)
+//setItem to put the high score in
+//put in as an array of objects
+//getItem to get it out
